89f3b7eeb6670265e0bfc7e83284077fcf7cf0c1
Nikita <nikitkamakarenko@gmail.com>
2023-12-11 19:52:49 +0300

    test content completely ready

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,9 +1,7 @@
 from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
 from notes.models import Note
 
 User = get_user_model()
